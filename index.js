13322cfda08deb89e2d1934a254ebd2797657401
caohuilin <1057275848@qq.com>
2020-04-27 16:24:49 +0800

    使用比例尺

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 function handleDocumentReady() {
+  demo1()
+  demo2()
+}
+// 简易柱状图
+function demo1() {
   const width = 300
   const height = 300
 
   // 添加画布
   const svg = d3
-    .select('body')
+    .select('.demo1')
     .append('svg')
     .attr('width', width)
     .attr('height', height)
@@ -19,9 +24,38 @@ function handleDocumentReady() {
     .append('rect')
     .attr('x', 20)
     .attr('y', (d) => d)
-    .attr('width', function (d) {
-      return d
-    })
+    .attr('width', (d) => d)
+    .attr('height', rectHeight - 2)
+    .attr('fill', 'steelblue')
+}
+
+// 带比例尺
+function demo2() {
+  const width = 300
+  const height = 300
+
+  // 添加画布
+  const svg = d3
+    .select('.demo2')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+
+  // 绘制矩形
+  const dataset = [1.2, 2.3, 0.9, 1.5, 3.3]
+  const min = d3.min(dataset)
+  const max = d3.max(dataset)
+
+  const linear = d3.scale.linear().domain([min, max]).range([0, 300])
+  const rectHeight = 25 // 每个矩形像素高度
+  svg
+    .selectAll('rect')
+    .data(dataset)
+    .enter()
+    .append('rect')
+    .attr('x', 20)
+    .attr('y', (_d, index) => index * (rectHeight + 5))
+    .attr('width', (d) => linear(d))
     .attr('height', rectHeight - 2)
     .attr('fill', 'steelblue')
 }
